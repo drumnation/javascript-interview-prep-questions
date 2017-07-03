58dd88ac3312408870359d6af0c15fa5be1c8ad9
drumnation <davidmieloch@gmail.com>
2017-07-03 15:57:42 -0400

    completed isSorted bi-directionally

--- 04-isSorted.js
+++ 04-isSorted.js
@@ -1,36 +1,47 @@
 /* 
 
-ISSORTED
+IS-SORTED
 
 Returns true or false, indicating whether the given array of numbers is sorted.
 
 ------------------------------------------------------------------------------------------*/
 
 /*
- * check the array is sorted
- * return: if positive ->  1
- *         if negative -> -1
- *         not sorted  ->  0
- */
 
-Array.prototype.isSorted = function() {
-  return (function(direction) {
-    return this.reduce(function(prev, next, i, arr) {
-      if (direction === undefined)
-        return (direction = prev <= next ? 1 : -1) || true;
-      else
-        return (direction + 1 ?
-          (arr[i-1] <= next) : 
-          (arr[i-1] >  next));
-    }) ? Number(direction) : false;
-  }).call(this);
+1. Pass array to check as argument
+2. isSorted changes array to a string '1,4,5'
+    a. uses sortArray to either sort array asc or desc, set by argument
+3. After sortArray sorts asc or desc compare against original and return bool
+
+*/
+
+const sortArray = (array=[], direction="ascending") => {
+    let copy = array.slice()
+    return (
+        direction === "ascending"
+            ? copy.sort( (a, b) => a - b )
+            : copy.sort( (a, b) => b - a )
+    )
 }
 
-var arr = [3,2,1,0];
-arr.isSorted();
+const isSorted = (array) => {
+    return (
+            array.toString() === sortArray(array, "ascending").toString() 
+        || 
+            array.toString() === sortArray(array, "descending").toString()
+    )
+}
 
 // TESTS 
 
-console.log(isSorted([])) // true
-console.log(isSorted([-Infinity, -5, 0, 3, 9])) // true
-console.log(isSorted([3, 9, -3, 10])) // false
+console.log(isSorted([1,4,5])); // true
+console.log(isSorted([ -10, -20, -30])); // true
+console.log(isSorted([0, -6, 10])); //false
+console.log(isSorted([0, 6, 10])); // true
+console.log(isSorted([6, 0, 10])); // false
+console.log(isSorted([ -10, -20, -30])) // true
+
+/*
+
+SOURCE: https://gist.github.com/ottoinfo/102fde6fef82a177fedf02fd1c470669
+------------------------------------------------------------------------------------*/
