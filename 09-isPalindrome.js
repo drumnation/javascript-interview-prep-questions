4e6e42af9803f7403f8d1b544911edbc170a0fa0
drumnation <davidmieloch@gmail.com>
2017-07-03 19:20:28 -0400

    setup isPalindrome

---
+++ 09-isPalindrome.js
@@ -0,0 +1,19 @@
+/* 
+
+ISPALINDROME
+
+Implement the indexOf function for arrays.
+
+------------------------------------------------------------------*/
+
+const isPalindrome = () => {
+    // code 
+}
+
+
+// TESTS
+
+console.log(isPalindrome('')) // true
+console.log(isPalindrome('abcdcba')) // true
+console.log(isPalindrome('abcd')) // false
+console.log(isPalindrome('A man a plan a canal Panama')) // true
