cf507f7322c5307e669eb1826ed2e0f09874037d
drumnation <davidmieloch@gmail.com>
2017-07-03 20:36:15 -0400

    solved isPalindrome with edge cases.

--- 09-isPalindrome.js
+++ 09-isPalindrome.js
@@ -2,14 +2,30 @@
 
 ISPALINDROME
 
-Implement the indexOf function for arrays.
+Return true or false indicating whether the given string is a 
+plaindrone (case and space insensitive).
 
-------------------------------------------------------------------*/
+------------------------------------------------------------------
 
-const isPalindrome = () => {
-    // code 
+1. Remove special characters, spaces, and make lowercase (The fourth test)
+2. Reverse formatChar for comparison
+3. Check to see if string is a Palindrome
+
+*/
+
+// WITH EDGE CASES
+
+const isPalindrome = (string) => {
+    const formatChar = string.replace(/[^A-Z0-9]/ig, "").toLowerCase()
+    const checkPalindrome = formatChar.split('').reverse().join('')
+    return (formatChar === checkPalindrome)
 }
 
+// SIMPLE
+
+function checkPalindrome(string) {
+    return string == string.split('').reverse().join('')
+}
 
 // TESTS
 
@@ -17,3 +33,8 @@ console.log(isPalindrome('')) // true
 console.log(isPalindrome('abcdcba')) // true
 console.log(isPalindrome('abcd')) // false
 console.log(isPalindrome('A man a plan a canal Panama')) // true
+
+/*
+
+SOURCE: https://stackoverflow.com/a/32106729/8245251
+------------------------------------------------------------------*/
