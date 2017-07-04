ea03da08e7d41d428f46b14b602211226fb39038
drumnation <davidmieloch@gmail.com>
2017-07-03 21:03:08 -0400

    in the middle of solving missing

--- 10-missing.js
+++ 10-missing.js
@@ -12,11 +12,53 @@ Hint: There’s a clever formula you can use.
 
 ------------------------------------------------------------------*/
 
+// const missing = (numArray) => {
+//     let missing = []
+//     for (let index = 0; index < numArray.length; index++) {
+//         if ((numArray[index + 1] - numArray[index]) > 1) {
+//             missing.push(numArray[index + 1] - numArray[1])   
+//         }
+//     }
+
+//     if (missing.length === 0) {
+//         return undefined
+//     } else {
+//         return missing
+//     }
+// }
+
+const missing = (numArray) => {
+    let missing = []
+    for (let index = 0; index < numArray.length; index++) {
+        if ((numArray[index + 1] - numArray[index]) > 1) {
+            missing.push(numArray[index + 1] - numArray[1])   
+        }
+    }
+    var arrayLength = Math.max.apply(Math, intArray);
+
+    for ( var i = 0; i < arrayLength; i++ ) {
+        if ( intArray.indexOf(i) < 0 ) {
+            missing.push( i );
+        }
+    }
+
+    if (missing.length === 0) {
+        return undefined
+    } else {
+        return missing
+    }
+}
+
 
 // TESTS
 
-console.log(missing([])) // undefined
-console.log(missing([1, 4, 3])) // 2
-console.log(missing([2, 3, 4])) // 1
-console.log(missing([5, 1, 4, 2])) // 3
-console.log(missing([1, 2, 3, 4])) // undefined
+console.log('[] (undefined) =>', missing([])) // undefined
+console.log('[1, 4, 3] (2) =>', missing([1, 4, 3])) // 2
+console.log('[2, 3, 4] (1) =>', missing( [2, 3, 4])) // 1
+console.log('[5, 1, 4, 2] (3) =>', missing([5, 1, 4, 2])) // 3
+console.log('[1, 2, 3, 4] (undefined) =>', missing([1, 2, 3, 4])) // undefined
+
+/*
+
+SOURCE: https://gist.github.com/amaxwell01/6108502
+------------------------------------------------------------------*/
