8e28561e69bdb21611748391a26e837d11286fe4
drumnation <davidmieloch@gmail.com>
2017-07-04 15:25:17 -0400

    finished prototype uniq

--- intermediate/03-uniq.js
+++ intermediate/03-uniq.js
@@ -6,12 +6,42 @@ Takes an array of numbers, and returns the unique numbers.
 
 Can you do it in O(N) time?
 
-------------------------------------------------------------------*/
+-------------------------------------------------------------------------
 
+SOLUTION STEPS
 
-const uniq = () => {
+1. Create a test object and a unique array to hold the deduped numbers
+2. Iterate through all array numbers
+    a. if test object item has a value that matches the current key
+        1. that means it already exists
+        2. ! bang means: if it doesn't exist
+    b. set value of test key equal to true 
+        1. so next time it will fail conditional
+    c. push this number for first time to unique number array
+3. Return the unique array
 
+*/
+
+// SOLUTION
+
+Array.prototype.uniq = () => {
+        let test = {}
+        let unique = []
+    for (let index = 0, len = this.length; index < len; index++) {
+        if (!test[this[index]]) {
+            test[this[index]] = true
+            unique.push(this[index])
+        }
+    }
+    return unique
 }
 
-console.log(uniq([])) // []
-console.log(uniq([1, 4, 2, 2, 3, 4, 8])) // [1, 4, 2, 3, 8]
+// TESTS
+
+console.log('([]) =>', [].uniq()) // []
+console.log('([ 1, 4, 2, 3, 8 ]) =>', [1, 4, 2, 2, 3, 4, 8].uniq()) // [1, 4, 2, 3, 8]
+
+/*
+
+SOURCE: https://gist.github.com/inkdeep/5101971
+-------------------------------------------------------------------------*/
