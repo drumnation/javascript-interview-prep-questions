72dfde4a11389fb958e9d130d516ea771b8c9b1b
drumnation <davidmieloch@gmail.com>
2017-07-04 17:23:48 -0400

    added solution

--- intermediate/07-assignDeep.js
+++ intermediate/07-assignDeep.js
@@ -14,18 +14,29 @@ SOLUTION STEPS
 
 // SOLUTION
 
-const assignDeep = (object1, object2) => {
-    
+const assignDeep = (target, source) => {
+    for (let key in source) {
+        if (source.hasOwnProperty(key)) {
+            let value = source[key]
+            if (typeof value === 'object' && value) {
+                target[key] = target[key] || {}
+                assignDeep(target[key], value)
+            } else {
+                target[key] = value
+            }
+        }
+    }
+    return target
 }
 
 // TESTS
 
-console.log(assignDeep({ a: 1 }, {}))                                      // { a: 1 }
-console.log(assignDeep({ a: 1 }, { a: 2 }))                                // { a: 2 }
-console.log(assignDeep({ a: 1 }, { a: { b: 2 } }))                         // { a: { b: 2 } }
-console.log(assignDeep({ a: { b: { c: 1 }}}, { a: { b: { d: 2 }}, e: 3 })) // { a: { b: { c: 1, d: 2 }}, e: 3 }
+console.log('({ a: 1 }) =>', assignDeep({ a: 1 }, {})) // { a: 1 }
+console.log('({ a: 2 }) =>', assignDeep({ a: 1 }, { a: 2 })) // { a: 2 }
+console.log('({ a: { b: 2 } }) =>', assignDeep({ a: 1 }, { a: { b: 2 } })) // { a: { b: 2 } }
+console.log('({ a: { b: { c: 1, d: 2 }}, e: 3 }) =>', assignDeep({ a: { b: { c: 1 }}}, { a: { b: { d: 2 }}, e: 3 })) // { a: { b: { c: 1, d: 2 }}, e: 3 }
 
 /*
 
-SOURCE: 
+SOURCE: https://www.reddit.com/r/javascript/comments/4g0904/objectassign_question/
 -----------------------------------------------------------------------------------------------*/
