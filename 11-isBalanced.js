69269a781df6377021854b2f610f2a8c727c3b55
drumnation <davidmieloch@gmail.com>
2017-07-04 12:42:58 -0400

    setup isBalanced

---
+++ 11-isBalanced.js
@@ -0,0 +1,28 @@
+/* 
+
+IS BALANCED
+
+Takes a string and returns true or false indicating whether its curly braces are balanced.
+
+------------------------------------------------------------------
+
+SOLUTION STEPS
+
+1.
+
+*/
+
+const isBalanced = () => {
+    
+}
+
+// TESTS
+
+console.log(isBalanced('foo { bar { baz } boo }')) // true
+console.log(isBalanced('foo { bar { baz }')) // false
+console.log(isBalanced('foo { bar } }')) // false
+
+/*
+
+SOURCE: 
+------------------------------------------------------------------*/
