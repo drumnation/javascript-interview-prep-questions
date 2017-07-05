7ca6bd91aed1093ee675e6f01d0d5e41d47add51
drumnation <davidmieloch@gmail.com>
2017-07-05 00:11:14 -0400

    solved seq

--- hard/seq.js
+++ hard/seq.js
@@ -2,33 +2,34 @@
 
 SEQ
 
-The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, . . . 
-Each subsequent number is the sum of the previous two.
+Resolve an array of promises in sequence (as opposed to Promise.all, which does it in parallel).
 
-Like the fib function you implemented above, able to handle numbers up to 50 (hint: look up memoization).
+------------------------------------------------------------------------------------------
 
-------------------------------------------------------------------------------------------*/
+SOLUTION STEPS
 
-// NON-RECURSIVELY WITH FOR LOOP
+1.
 
-/*
-
-1. Start our loop at count = 2 since fib number is the sum of the previous 2. (3rd item)
-2. a is dropped off and replaced with b.
-3. b is replaced with the current index value of the sequence, being our new sum.
-4. When loop reaches number index, return new sum value
 
 */
 
-const resolve = (num) => {}
+// SOLUTION
 
+
+const seq = async (promisesArray) => {
+    try {
+        const sequence = await promisesArray.map( promise => promise )
+        console.log(sequence)
+    } catch (err) {
+        console.log(err)
+    }
+}
 // TESTS
-
 let a = Promise.resolve('a')
 let b = Promise.resolve('b')
 let c = Promise.resolve('c')
-await console.log(seq([a, b, c])) // ['a', 'b', 'c']
-await console.log(seq([a, c, b])() // ['a', 'c', 'b']
+seq([a, b, c]) // ['a', 'b', 'c']
+seq([a, c, b]) // ['a', 'c', 'b']
 
 /*
 
